a304bc102029f4352dc42b2453c6c35bd8765d4b
William Tavares <prevensulwilliam@gmail.com>
2026-04-20 00:51:49 -0300

    chore(cards): remove import ocioso GoldButton

--- src/pages/CardsPage.tsx
+++ src/pages/CardsPage.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { supabase } from "@/integrations/supabase/client";
-import { GoldButton } from "@/components/wt7/GoldButton";
 import { PremiumCard } from "@/components/wt7/PremiumCard";
 import { toast } from "sonner";
 
